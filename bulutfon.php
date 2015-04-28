f981cfa427abecc570c8d54c11fbfb11db1898be
Hakan ERSU <hakanersu@gmail.com>
2015-04-28 16:19:53 +0300

    indentation fix

--- bulutfon.php
+++ bulutfon.php
@@ -1,95 +1,95 @@
 <?php
-	require "HipChat.php";
+require "HipChat.php";
 
-	require "configuration.php";
-	
-	//HipChat API token.
-	$hcToken = "hipchat_api_token";
-	
-	// WHMCS panelinizde kullanicinin telefonun bulundugu alan id'si.
-	$fieldID = 5;
+require "configuration.php";
 
-	// HipChat'te kullacagimiz oda ismi
-	$roomName = 'HIPCHAT ROOM NAME';
+//HipChat API token.
+$hcToken = "hipchat_api_token";
 
-	// Mesaj gonderen ismi.
-	$from = 'BULUTFON';
+// WHMCS panelinizde kullanicinin telefonun bulundugu alan id'si.
+$fieldID = 5;
 
-	// Karsilama menusu id'si
-	$welcome = 8;
+// HipChat'te kullacagimiz oda ismi
+$roomName = 'HIPCHAT ROOM NAME';
 
-	// WHMCS admin paneli urlsi
-	$url = 'http://www.adres.com/admin/';
+// Mesaj gonderen ismi.
+$from = 'BULUTFON';
 
-	function json($array){
-		header('Content-Type: application/json');
-		die(json_encode($array));
-	}
+// Karsilama menusu id'si
+$welcome = 8;
 
-	// Bulutfon uzerinden bize gonderilen telefon numaralari ulke kodu ile gonderilmekte
-	// veritabanimizda telefon numaralarinin benzer formatta olmadini varsayarak
-	// gonderilen telefon numarasindan 90 ibaresini kaldiralim.
-	$caller = isset($_POST['caller']) ? ltrim($_POST['caller'],'90') : false;
+// WHMCS admin paneli urlsi
+$url = 'http://www.adres.com/admin/';
 
-	// BulutfonXM icin panelleri uzerinde yetkilendirme secenegi olsada basit
-	// bir hash ile guvenligimizi biraz daha artirabiliriz.
-	$hash = isset($_GET['hash']) ? $_GET['hash'] : false;
+function json($array){
+	header('Content-Type: application/json');
+	die(json_encode($array));
+}
 
-	if($hash!='HASH' || !$caller) json(['error'=>'parameters missing']);
+// Bulutfon uzerinden bize gonderilen telefon numaralari ulke kodu ile gonderilmekte
+// veritabanimizda telefon numaralarinin benzer formatta olmadini varsayarak
+// gonderilen telefon numarasindan 90 ibaresini kaldiralim.
+$caller = isset($_POST['caller']) ? ltrim($_POST['caller'],'90') : false;
 
-	$conn = new PDO("mysql:host=localhost;dbname={$db_name}",$db_username,$db_password);
+// BulutfonXM icin panelleri uzerinde yetkilendirme secenegi olsada basit
+// bir hash ile guvenligimizi biraz daha artirabiliriz.
+$hash = isset($_GET['hash']) ? $_GET['hash'] : false;
 
-	// Gonderilen veriye bfxm formatinda cevap vermemiz gerekmekte.
-	// Herhangi bir zincirleme islem( kullanicidan bir tusa basmasini istemek ) yapmayacagimizdan 
-	// kullaniciyi karsilama menu grubuna yonlendirebilirz.
-	$cevap = array(
-		"bfxm"=>array("version"=>1),
-		"seq"=>array(
-			array(
-				"action"=>"dial",
-				// 8 bizim yonlendirecegimiz grup numarasi.
-				"args"=>array("destination"=>$welcome)
-			)
+if($hash!='HASH' || !$caller) json(['error'=>'parameters missing']);
+
+$conn = new PDO("mysql:host=localhost;dbname={$db_name}",$db_username,$db_password);
+
+// Gonderilen veriye bfxm formatinda cevap vermemiz gerekmekte.
+// Herhangi bir zincirleme islem( kullanicidan bir tusa basmasini istemek ) yapmayacagimizdan 
+// kullaniciyi karsilama menu grubuna yonlendirebilirz.
+$cevap = array(
+	"bfxm"=>array("version"=>1),
+	"seq"=>array(
+		array(
+			"action"=>"dial",
+			// 8 bizim yonlendirecegimiz grup numarasi.
+			"args"=>array("destination"=>$welcome)
 		)
-	);
+	)
+);
+
+// WHMCS uzerinden telefon numarasina gore kullaniciyi bulalim. 
+$query = $conn->prepare("SELECT tblclients.id,
+		tblclients.firstname,
+		tblclients.lastname,
+		tblcustomfieldsvalues.value AS telefon 
+	FROM tblcustomfieldsvalues 
+	LEFT JOIN tblclients ON tblclients.id=tblcustomfieldsvalues.relid 
+	WHERE tblcustomfieldsvalues.value LIKE ? AND tblcustomfieldsvalues.fieldid=?");
+
+$query->execute(["%$caller%",$fieldID]);
+
+$profile = $query->fetch(PDO::FETCH_OBJ);
+
+if($profile){
 	
-	// WHMCS uzerinden telefon numarasina gore kullaniciyi bulalim. 
-	$query = $conn->prepare("SELECT tblclients.id,
-			tblclients.firstname,
-			tblclients.lastname,
-			tblcustomfieldsvalues.value AS telefon 
-		FROM tblcustomfieldsvalues 
-		LEFT JOIN tblclients ON tblclients.id=tblcustomfieldsvalues.relid 
-		WHERE tblcustomfieldsvalues.value LIKE ? AND tblcustomfieldsvalues.fieldid=?");
+	// Kullanicimiz varsa kullanicinin son 5 destek biletine ulasalim.
+	$query = $conn->prepare("SELECT id,title FROM tbltickets WHERE userid=? AND status!='Closed' ORDER BY id DESC LIMIT 5");
 
-	$query->execute(["%$caller%",$fieldID]);
+	$query->execute([$profile->id]);
 
-	$profile = $query->fetch(PDO::FETCH_OBJ);
+	$tickets= $query->fetchAll(PDO::FETCH_OBJ);
 
-	if($profile){
-		
-		// Kullanicimiz varsa kullanicinin son 5 destek biletine ulasalim.
-		$query = $conn->prepare("SELECT id,title FROM tbltickets WHERE userid=? AND status!='Closed' ORDER BY id DESC LIMIT 5");
+	$liste= "";
 
-		$query->execute([$profile->id]);
+	if($tickets){
+		$liste = "<ul>";
 
-		$tickets= $query->fetchAll(PDO::FETCH_OBJ);
-
-		$liste= "";
-
-		if($tickets){
-			$liste = "<ul>";
-
-			foreach($tickets as $p){
-				$liste.="<br><li> <a href='{$url}supporttickets.php?action=view&id={$p->id}'>{$p->title}</a></li>";
-			}
-
-			$liste .="</ul>";
+		foreach($tickets as $p){
+			$liste.="<br><li> <a href='{$url}supporttickets.php?action=view&id={$p->id}'>{$p->title}</a></li>";
 		}
 
-		$hc = new HipChat\HipChat($hcToken);
-
-		$hc->message_room($roomName,$from,"<b>{$profile->firstname}  {$profile->lastname} </b> ({$caller}) arıyor. {$liste}",true,'random');
+		$liste .="</ul>";
 	}
 
-	json($cevap);
+	$hc = new HipChat\HipChat($hcToken);
+
+	$hc->message_room($roomName,$from,"<b>{$profile->firstname}  {$profile->lastname} </b> ({$caller}) arıyor. {$liste}",true,'random');
+}
+
+json($cevap);
